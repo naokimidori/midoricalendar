dd487fa9870949e24f755521f6009b46862d1053
vino <naokimidori@163.com>
2020-05-30 23:15:47 +0800

    feat: 完善我的页面

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -13,6 +13,19 @@ const options = {
       url: '/pages/guide/guide'
     });
   },
+
+  handleSetting() {
+    wx.navigateTo({
+      url: '/pages/sheet-setting/sheet-setting'
+    });
+  },
+
+  showZScode: async function () {
+    wx.previewImage({
+      urls: ['cloud://calendar-prod-w7kpb.6361-calendar-prod-w7kpb-1302089220/project_pics/zanshang.jpg'],
+      current: 'cloud://calendar-prod-w7kpb.6361-calendar-prod-w7kpb-1302089220/project_pics/zanshang.jpg',
+    });
+  },
 };
 
 create.Page(store, options);
