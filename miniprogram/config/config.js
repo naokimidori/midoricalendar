2380cb9d8af89eb18aaa0b588bbb18c1f0994274
vino <naokimidori@163.com>
2020-05-31 19:17:59 +0800

    fix: 代码优化

---
+++ miniprogram/config/config.js
@@ -0,0 +1,10 @@
+export default {
+  miniApp: '小树日历',
+  miniAppDesc: '一款可以让你提高生活效率的日历',
+  miniSlogan: '在最美的年龄为最纯真的梦做最大的努力',
+  version: '1.0.0',
+  amapGeoUrl: 'https://restapi.amap.com/v3/geocode/regeo',
+  amapKey: 'aadcb3316c0472d20f026b75e326c34a',
+  tipQrCode: 'cloud://calendar-prod-w7kpb.6361-calendar-prod-w7kpb-1302089220/project_pics/zanshang.jpg',
+  logoGif: 'https://cdn.nlark.com/yuque/0/2019/gif/280373/1567733825085-assets/web-upload/8f327547-55d1-4397-aeba-8ae68309d0fb.gif',
+}
