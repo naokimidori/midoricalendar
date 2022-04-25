a994433cb060afee9fd4cdf3cddc572bab026c40
zhaolong <naokimidori@163.com>
2022-04-25 22:26:41 +0800

    feat: theme purple

--- miniprogram/config/config.js
+++ miniprogram/config/config.js
@@ -8,3 +8,5 @@ export default {
   tipQrCode: 'cloud://calendar-prod-w7kpb.6361-calendar-prod-w7kpb-1302089220/project_pics/zanshang.jpg',
   logoGif: 'https://cdn.nlark.com/yuque/0/2019/gif/280373/1567733825085-assets/web-upload/8f327547-55d1-4397-aeba-8ae68309d0fb.gif',
 }
+
+export const themeColor = '#6739b6';
